8b38e02749f349e34ca7706aea94fe2cecf2a080
AugustusFaren <38141595+AugustusFaren@users.noreply.github.com>
2019-05-14 21:16:13 +0530

    Update switch calculator.cpp

--- switch calculator.cpp
+++ switch calculator.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-     cout <<"Enter Your Option Below, " << endl;
+    cout <<"Enter Your Option Below, " << endl;
     cout <<"\t1.Add " << endl;
     cout <<"\t2.Subract " << endl;
     cout << "\t3.Multiply"<< endl;
@@ -20,24 +20,27 @@ int main()
 
     cout <<" Your Option : " << flush;
     cin>> Option;
-    switch(Option){
-case 1:
-{
-    int Add1;
-    int Add2;
+    switch(Option)
+    {
+    case 1:
+    {
+
+
+        int Add1;
+        int Add2;
 
         cout <<" Enter the first number :" << flush;
         cin>> Add1;
         cout <<"Enter the second number :" << flush;
         cin>> Add2;
-int Add3=Add1+Add2;
+        int Add3=Add1+Add2;
         cout<<"Answer = "<< Add3<<endl;
-
-}break;
-case 2:
- {
-     int Subract1;
-    int Subract2;
+    }
+    break;
+    case 2:
+    {
+        int Subract1;
+        int Subract2;
 
         cout <<" Enter the first number :" << flush;
         cin>> Subract1;
@@ -46,33 +49,38 @@ case 2:
         int Subract3=Subract1-Subract2;
         cout<<"Answer = "<< Subract3<<endl;
 
- }
-        break;
-case 3:
- {
-     long long int Multiply1;
-    long long int Multiply2;
+    }
+    break;
+    case 3:
+    {
+        long long int Multiply1;
+        long long int Multiply2;
 
-         cout <<" Enter the first number :" << flush;
+        cout <<" Enter the first number :" << flush;
         cin>> Multiply1;
         cout <<"Enter the second number :" << flush;
         cin>> Multiply2;
         long double Multiply3=Multiply1*Multiply2;
         cout<<"Answer = "<<setprecision(0)<<fixed<<Multiply3<<endl;
- }        break;
+    }
+    break;
 
-case 4:
-    {float Divide1;
-    float Divide2;
+    case 4:
+    {
+        float Divide1;
+        float Divide2;
 
-         cout <<" Enter the first number :" << flush;
+        cout <<" Enter the first number :" << flush;
         cin>> Divide1;
         cout <<"Enter the second number :" << flush;
         cin>> Divide2;
         float Divide3=Divide1/Divide2;
         cout<<"Answer = "<< Divide3<<endl;
-       } break;
+    }
+    break;
+    default:
+        cout <<"Please enter a correct option "<< endl;
 
-}
-return 0;
+    }
+    return 0;
 }
