4c3e2f1ac0633becc7487ad7d342ead3ea6b6ba6
AugustusFaren <38141595+AugustusFaren@users.noreply.github.com>
2019-05-14 20:12:52 +0530

    This one will work properly.

--- switch calculator.cpp
+++ switch calculator.cpp
@@ -13,53 +13,65 @@ int main()
     int Multiply;
     int Divide;
     int Option;
-    int Add1;
-    int Add2;
-    int Add3=Add1+Add2;
-    int Subract1;
-    int Subract2;
-    int Subract3=Subract1-Subract2;
-    long long int Multiply1;
-    long long int Multiply2;
-    long double Multiply3=Multiply1*Multiply2;
-    float Divide1;
-    float Divide2;
-    float Divide3=Divide1/Divide2;
+
+
+
+
+
     cout <<" Your Option : " << flush;
     cin>> Option;
     switch(Option){
 case 1:
+{
+    int Add1;
+    int Add2;
 
         cout <<" Enter the first number :" << flush;
         cin>> Add1;
         cout <<"Enter the second number :" << flush;
         cin>> Add2;
-
+int Add3=Add1+Add2;
         cout<<"Answer = "<< Add3<<endl;
-    break;
+
+}break;
 case 2:
+ {
+     int Subract1;
+    int Subract2;
+
         cout <<" Enter the first number :" << flush;
         cin>> Subract1;
         cout <<"Enter the second number :" << flush;
         cin>> Subract2;
+        int Subract3=Subract1-Subract2;
         cout<<"Answer = "<< Subract3<<endl;
 
+ }
         break;
 case 3:
+ {
+     long long int Multiply1;
+    long long int Multiply2;
+
          cout <<" Enter the first number :" << flush;
         cin>> Multiply1;
         cout <<"Enter the second number :" << flush;
         cin>> Multiply2;
+        long double Multiply3=Multiply1*Multiply2;
         cout<<"Answer = "<<setprecision(0)<<fixed<<Multiply3<<endl;
-        break;
+ }        break;
 
 case 4:
+    {float Divide1;
+    float Divide2;
+
          cout <<" Enter the first number :" << flush;
         cin>> Divide1;
         cout <<"Enter the second number :" << flush;
         cin>> Divide2;
+        float Divide3=Divide1/Divide2;
         cout<<"Answer = "<< Divide3<<endl;
-        break;
+       } break;
 
 }
 return 0;
